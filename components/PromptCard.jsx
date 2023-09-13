b1a13643ebddee39c5ca12384003c2d814fcd2bb
Muhammad Farhan <gfarhan18@gmail.com>
2023-09-13 15:18:55 +0500

    worked on crud operation of posts

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,15 @@
 "use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-const PromptCard = ({ post, handleTagClick, handleEidt, handleDelete }) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState(""); 
+  const { data: session } = useSession();
+  const pathName= usePathname();
+  const router=useRouter();
+
   const handleCopy = () => {
     navigator.clipboard.writeText(post.prompt);
     setCopied(post.prompt);
@@ -49,6 +55,15 @@ const PromptCard = ({ post, handleTagClick, handleEidt, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName ==='/profile' &&
+      (<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+        <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+          Edit
+        </p>
+        <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
+          Delete
+        </p>
+      </div>)}
     </div>
   );
 };
